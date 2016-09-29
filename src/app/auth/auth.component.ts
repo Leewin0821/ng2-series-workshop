7c8f6e2a4d1ab06d40caffe56c1a8dcdc86023ea
lwzhang <lwzhang@thoughtworks.com>
2016-09-29 19:01:57 +0800

    [Leewin] Added signIn and signUp function

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {link} from 'fs'
+
+const linkTexts = {
+    signIn : `Don't have an account?`,
+    signUp: 'Already have an account?'
+}
 
 @Component({
-    template: require('./auth.component.html')
+    template: require('./auth.component.html'),
+    styles: [require('./auth.component.css')]
 })
 export class AuthComponent implements OnInit {
+    user: {
+        email: string,
+        password: string
+    }
+
+    mode: 'signIn' | 'signUp'
+    linkText: string
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.user = {
+            email: '',
+            password: ''
+        }
+        this.mode = 'signIn'
+        this.linkText = linkTexts.signIn
+    }
 
+    changeMode(): void {
+        if (this.mode === 'signIn') {
+            this.mode = 'signUp'
+            this.linkText = linkTexts.signUp
+        } else {
+            this.mode = 'signIn'
+            this.linkText = linkTexts.signIn
+        }
+    }
 }
